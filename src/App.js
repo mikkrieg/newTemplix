adf9ddcc93f909d226e1792fd01f9e3d9db51f87
Mikey Kriegel <mikkrieg@gmail.com>
2021-11-18 13:59:23 -0800

    Add logo, add basic app, remove uneeded files, add packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+// import { AppProvider } from './AppContext.js';
+import './sass/main.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <AppProvider>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/layouts' component={Layouts} />
+        </Switch>
+        <Footer/>
+      </Router>
+    // {/* </AppProvider> */}
   );
 }
+
+export default App;
 
 export default App;
