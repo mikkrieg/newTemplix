fb884b814b39adc12ddad3b630fa152c2ae22e12
Mikey Kriegel <mikkrieg@gmail.com>
2021-12-23 10:56:37 -0800

    Update react router history

--- src/components/layouts/Sports.js
+++ src/components/layouts/Sports.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect} from 'react';
-import { useNavigate } from 'react-router'
+import { useHistory } from 'react-router'
 import { AppContext } from '../../AppContext';
 import InfoModal from '../InfoModal';
 import '../../styles/css/layouts/sports.css';
@@ -8,7 +8,7 @@ const Sports = () => {
   const { setOnProject } = useContext(AppContext);
   const [ open, setOpen ] = useState(false);
   const [ click, setClick ] = useState(false);
-  const navigate = useNavigate();
+  const history = useHistory();
   
   useEffect(() => {
     setOnProject(true);
@@ -18,7 +18,7 @@ const Sports = () => {
     setOnProject(false);
     document.body.classList.add('background');
     document.body.classList.add('background--color-map');
-    navigate('/layouts');
+    history.push('/layouts');
   }
 
   const activate = () => {
